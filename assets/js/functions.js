c94eebc091320e278dd98012e7d440ebd44de2a9
adissontejo <adissonmprtejo@gmail.com>
2022-07-19 04:26:14 -0300

    Add avaliations

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -150,3 +150,108 @@ $(window).scroll(function () {
         $(".page-up > span").attr("style", "display: none");
 	}      
 });
+
+const avaliationsRef = db.ref('/avaliations');
+
+const avaliationsValue = {};
+
+avaliationsRef.on('value', snapshot => {
+    const data = snapshot.val();
+
+    Object.assign(avaliationsValue, data);
+
+    Object.entries(data).forEach(([key, value]) => {
+        if (key === 'total') {
+            return;
+        }
+
+        const avaliation = document.getElementById(`${key}-avaliation`);
+
+        avaliation.setAttribute('style', `width: ${value * 10}%;`);
+    });
+});
+
+const avaliateButton = document.getElementById('avaliate-button');
+const avaliateAlert = document.getElementById('avaliate-alert');
+const avaliateAlertWrapper = document.getElementById('avaliate-alert-wrapper');
+
+avaliateButton.addEventListener('click', () => {
+    avaliateAlert.classList.add('open');
+});
+
+avaliateAlertWrapper.addEventListener('click', e => {
+    e.stopPropagation();
+});
+
+avaliateAlert.addEventListener('click', () => {
+    avaliateAlert.classList.remove('open');
+});
+
+const avaliationSteps = avaliateAlertWrapper.getElementsByClassName('avaliation-step');
+
+const avaliation = JSON.parse(localStorage.getItem('avaliation')) || {};
+
+for (let i = 0; i < avaliationSteps.length; i++) {
+    const step = avaliationSteps.item(i);
+
+    const grades = step.getElementsByClassName('grades').item(0);
+
+    for (let j = 0; j <= 10; j++) {
+        const gradeButton = document.createElement('button');
+        gradeButton.id = `${step.id}-${j}`;
+        gradeButton.textContent = j;
+        gradeButton.classList.add('grade-button');
+
+        if (avaliation[step.id] === j) {
+            gradeButton.classList.add('selected');
+        }
+
+        gradeButton.addEventListener('click', () => {
+            const prev = document.getElementById(`${step.id}-${avaliation[step.id]}`);
+
+            if (prev) {
+                prev.classList.remove('selected');
+            }
+
+            avaliation[step.id] = j;
+            
+            gradeButton.classList.add('selected');
+        });
+
+        grades.appendChild(gradeButton);
+    }
+}
+
+const sendAvaliationButton = document.getElementById('send-avaliation-button');
+
+sendAvaliationButton.addEventListener('click', () => {
+    if (
+        avaliation.methodology !== undefined &&
+        avaliation.dificulty !== undefined &&
+        avaliation.general !== undefined
+    ) {
+        const oldAvaliation = JSON.parse(localStorage.getItem('avaliation'));
+
+        if (oldAvaliation) {
+            Object.entries(avaliation).forEach(([key, value]) => {
+                const prev = avaliationsValue[key]*avaliationsValue.total;
+
+                avaliationsValue[key] = (prev - oldAvaliation[key] + avaliation[key])/avaliationsValue.total;
+            });
+        } else {
+            Object.entries(avaliation).forEach(([key, value]) => {
+                const prev = avaliationsValue[key]*avaliationsValue.total;
+    
+                avaliationsValue[key] = (prev + value)/(avaliationsValue.total + 1);
+            });
+    
+            avaliationsValue.total = avaliationsValue.total + 1;
+        }
+
+        avaliationsRef.set(avaliationsValue);
+
+        localStorage.setItem('avaliation', JSON.stringify(avaliation));
+
+        avaliateAlert.classList.remove('open');
+    }
+});

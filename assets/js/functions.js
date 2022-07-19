31ec09e2e7362ba1c2419eb8525ac8ce0b995b01
Ana <anacarolinaneta@hotmail.com>
2022-07-18 21:33:35 -0300

    adicionei habilidades

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -60,4 +60,16 @@ submitQuestionButton.addEventListener('click', () => {
             answer: '',
         });
     }
-})
+});
+
+
+$('.header a[href^="#"]').on('click', function(e) {
+    e.preventDefault();
+    var id = $(this).attr('href'),
+    targetOffset = $(id).offset().top;
+      
+    $('html, body').animate({ 
+      scrollTop: targetOffset
+    }, 500);
+});
+
